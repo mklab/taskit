f1245c13182b404080fbc0c485b1bdef243327af
yuhi <yuhi@ubuntu>
2011-01-21 14:45:45 +0900

    Taskitのテストを作成しました。

---
+++ src/test/java/org/mklab/taskit/client/GwtTestTaskit.java
@@ -0,0 +1,42 @@
+package org.mklab.taskit.client;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.junit.client.GWTTestCase;
+import com.google.gwt.user.client.rpc.AsyncCallback;
+
+/**
+ * @author Yuhi Ishikura
+ * @version $Revision$, Jan 21, 2011
+ */
+public class GwtTestTaskit extends GWTTestCase {
+
+	/**
+	 * @see com.google.gwt.junit.client.GWTTestCase#getModuleName()
+	 */
+	@Override
+	public String getModuleName() {
+		return "org.mklab.taskit.taskitTest"; //$NON-NLS-1$
+	}
+
+	/**
+	 * データベースへのアクセスがうまくいったかどうかテストします。
+	 */
+	public void testDbAccess() {
+		final DBSampleServiceAsync service = GWT.create(DBSampleService.class);
+
+		delayTestFinish(1000 * 10);
+		service.accessToDatabase(new AsyncCallback<String>() {
+
+			@SuppressWarnings("synthetic-access")
+			public void onSuccess(String result) {
+				assertNotNull(result);
+				finishTest();
+			}
+
+			public void onFailure(Throwable caught) {
+				fail(caught.toString());
+			}
+		});
+	}
+
+}
