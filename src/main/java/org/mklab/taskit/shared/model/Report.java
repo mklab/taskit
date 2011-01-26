bd53bfb805489a9a7a12d6bf251a42caadbb4fb8
teshima <teshima@ubuntu>
2011-01-26 16:59:11 +0900

    主キーに@Idを付加しました。

--- src/main/java/org/mklab/taskit/shared/model/Report.java
+++ src/main/java/org/mklab/taskit/shared/model/Report.java
@@ -4,6 +4,7 @@
 package org.mklab.taskit.shared.model;
 
 import javax.persistence.Entity;
+import javax.persistence.Id;
 
 import net.sf.gilead.pojo.gwt.LightEntity;
 
@@ -16,9 +17,10 @@ import net.sf.gilead.pojo.gwt.LightEntity;
 public final class Report extends LightEntity {
 
   /** */
-  private static final long serialVersionUID = 1L;
+  private static final long serialVersionUID = 28409820481L;
 
   /** 課題のID */
+  @Id
   private String id;
   /** 課題の名前 */
   private String name;
@@ -30,6 +32,7 @@ public final class Report extends LightEntity {
   private String level;
   /** 課題の配点 */
   private String allotment;
+
   /**
    * {@link Report}オブジェクトを構築します。
    * 
@@ -49,102 +52,113 @@ public final class Report extends LightEntity {
     this.setLevel(level);
     this.setAllotment(allotment);
   }
+
   /**
    * 課題IDを設定します。
-   *
+   * 
    * @param id 課題ID
    */
   public void setId(String id) {
     this.id = id;
   }
+
   /**
-   * 課題IDを取得します。
-  　*
+   * 課題IDを取得します。 　*
+   * 
    * @return 課題ID
    */
   public String getId() {
     return this.id;
   }
+
   /**
    * 課題の名前を設定します。
-   *
+   * 
    * @param name 課題の名前
    */
   public void setName(String name) {
     this.name = name;
   }
+
   /**
-   * 課題の名前を取得します。
-  　*
+   * 課題の名前を取得します。 　*
+   * 
    * @return 課題の名前
    */
   public String getName() {
     return this.name;
   }
+
   /**
    * 課題の詳細説明を設定します。
-   *
+   * 
    * @param detail 課題の詳細説明
    */
   public void setDetail(String detail) {
     this.detail = detail;
   }
+
   /**
-   * 課題の詳細説明を取得します。
-  　*
+   * 課題の詳細説明を取得します。 　*
+   * 
    * @return 課題の詳細説明
    */
   public String getDetail() {
     return this.detail;
   }
+
   /**
    * 課題の出された日付を設定します。
-   *
+   * 
    * @param date 課題の出された日付
    */
   public void setDate(String date) {
     this.date = date;
   }
+
   /**
-   * 課題の出された日付を取得します。
-  　*
+   * 課題の出された日付を取得します。 　*
+   * 
    * @return 課題の出された日付
    */
   public String getDate() {
     return this.date;
   }
+
   /**
    * 課題のレベル（番号）を設定します。
-   *
+   * 
    * @param level 課題のレベル（番号）
    */
   public void setLevel(String level) {
     this.level = level;
   }
+
   /**
-   * 課題のレベル（番号）を取得します。
-  　*
+   * 課題のレベル（番号）を取得します。 　*
+   * 
    * @return 課題のレベル（番号）
    */
   public String getLevel() {
     return this.level;
   }
+
   /**
    * 課題の配点を設定します。
-   *
+   * 
    * @param allotment 課題の配点
    */
   public void setAllotment(String allotment) {
     this.allotment = allotment;
   }
+
   /**
-   * 課題の配点を取得します。
-  　*
+   * 課題の配点を取得します。 　*
+   * 
    * @return 課題の配点
    */
   public String getAllotment() {
     return this.allotment;
   }
-  
 
 }
