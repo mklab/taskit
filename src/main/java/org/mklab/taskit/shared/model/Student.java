40b69536fc8ca8cddd24b13cd84a816825925a24
yuhi <yuhi@hiyoko-taskit>
2011-01-31 16:43:33 +0900

    Studentテーブルの再設計を行ないました。

--- src/main/java/org/mklab/taskit/shared/model/Student.java
+++ src/main/java/org/mklab/taskit/shared/model/Student.java
@@ -4,7 +4,10 @@
 package org.mklab.taskit.shared.model;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+
+import net.sf.gilead.pojo.gwt.LightEntity;
 
 
 /**
@@ -12,12 +15,71 @@ import javax.persistence.Id;
  * @version $Revision$, Jan 28, 2011
  */
 @Entity(name = "STUDENT")
-public class Student {
+public class Student extends LightEntity {
+
+  /** for serialization. */
+  private static final long serialVersionUID = 300937258727774557L;
 
   /** アカウントのIDです。 */
   @Id
-  private String accountId;
+  @GeneratedValue
+  private int accountId;
 
   /** 学籍番号です。 */
   private String studentNo;
+
+  /**
+   * {@link Student}オブジェクトを構築します。
+   * 
+   * @param studentNo 学籍番号
+   */
+  public Student(String studentNo) {
+    this.studentNo = studentNo;
+  }
+
+  /**
+   * {@link Student}オブジェクトを構築します。
+   * <p>
+   * for gwt serialization.
+   */
+  Student() {
+    // do nothing
+  }
+
+  /**
+   * accountIdを取得します。
+   * 
+   * @return accountId
+   */
+  public int getAccountId() {
+    return this.accountId;
+  }
+
+  /**
+   * accountIdを設定します。
+   * 
+   * @param accountId accountId
+   */
+  public void setAccountId(int accountId) {
+    this.accountId = accountId;
+  }
+
+  /**
+   * studentNoを取得します。
+   * 
+   * @return studentNo
+   */
+  public String getStudentNo() {
+    return this.studentNo;
+  }
+
+  /**
+   * studentNoを設定します。
+   * 
+   * @param studentNo studentNo
+   */
+  public void setStudentNo(String studentNo) {
+    this.studentNo = studentNo;
+  }
+
 }
