1dad396eb24ea9c96d9aa8f4f5bccb24d6489d4e
yuhi <yuhi@hiyoko-taskit>
2011-02-01 20:14:52 +0900

    AttendanceServiceインターフェースを変更しました。

--- src/main/java/org/mklab/taskit/shared/service/AttendanceService.java
+++ src/main/java/org/mklab/taskit/shared/service/AttendanceService.java
@@ -4,7 +4,6 @@
 package org.mklab.taskit.shared.service;
 
 import org.mklab.taskit.shared.dto.AttendanceDto;
-import org.mklab.taskit.shared.model.Lecture;
 
 
 /**
@@ -19,17 +18,17 @@ public interface AttendanceService {
    * ユーザーの指定された講義の出席種別を設定します。
    * 
    * @param userName ユーザー名
-   * @param lecture 講義
+   * @param lectureIndex 講義
    * @param attendanceType 出席種別
    */
-  void setAttendanceType(String userName, Lecture lecture, String attendanceType);
+  void setAttendanceType(String userName, int lectureIndex, String attendanceType);
 
   /**
    * 指定された講義の、学生の出席状況を全て取得します。
    * 
-   * @param lecture 講義
+   * @param lectureIndex 講義
    * @return 出席状況
    */
-  AttendanceDto[] getAttendanceTypesOfStudents(Lecture lecture);
+  AttendanceDto[] getAttendanceTypesOfStudents(int lectureIndex);
 
 }
