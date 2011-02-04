9b1d03b6867d55925f39a9b59e509bd50b336203
teshima <teshima@mk.ces.kyutech.ac.jp>
2011-02-04 17:19:12 +0900

    SubmissionDaoに全生徒の提出データを取得するためのメソッドをインターフェースに定義しました。未実装です。

--- src/main/java/org/mklab/taskit/server/dao/StudentDaoImpl.java
+++ src/main/java/org/mklab/taskit/server/dao/StudentDaoImpl.java
@@ -3,8 +3,12 @@
  */
 package org.mklab.taskit.server.dao;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.persistence.Query;
+
+import org.mklab.taskit.shared.model.Submission;
 
 
 /**
@@ -32,6 +36,15 @@ public class StudentDaoImpl implements StudentDao {
     Query query = this.entityManager.createQuery("select accountId from STUDENT where accountId = " + accountId); //$NON-NLS-1$
     String studentNo = (String)query.getSingleResult();
     return studentNo;
+  }
+
+  /**
+   * @see org.mklab.taskit.server.dao.StudentDao#getAllStudentsSubmissionFromLectureId(int)
+   */
+  @Override
+  public List<Submission> getAllStudentsSubmissionFromLectureId(int lectureId) {
+    // TODO 未実装です。
+    return null;
   }
 
 }
